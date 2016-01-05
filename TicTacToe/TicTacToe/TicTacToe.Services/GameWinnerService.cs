850f1c587a3f0c1a0c3ca00b3de12ca6f7c239e2
puran1218 <zxw_kelvin@hotmail.com>
2016-01-06 01:07:04 +0800

    First NUnit test

---
+++ TicTacToe/TicTacToe/TicTacToe.Services/GameWinnerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe.Services
+{
+    public interface IGameWinnerService
+    {
+        char Validate(char[,] gameBoard);
+    }
+
+    public class GameWinnerService : IGameWinnerService
+    {
+        public char Validate(char[,] gameBoard)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
